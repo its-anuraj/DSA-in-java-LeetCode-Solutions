4f6bce084448406c539c067cf4c9488d5e19527d
Anuraj singh <ajsinghindolia@gmail.com>
2026-08-04 17:54:41 +0530

    Time: 0 ms (100%), Space: 43.1 MB (99.47%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,28 +1,66 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int i = 0;
-        int j = nums.length - 1;
-        while (i <= j) {
-            int mid = i + (j - i) / 2;
-            if (nums[mid] == target)
+
+        int pivot = FindPivotInArray(nums);
+
+       
+        if (pivot == -1) {
+            return FindTarget(nums, 0, nums.length - 1, target);
+        }
+
+         if (nums[pivot] == target) {
+            return pivot;
+        }
+
+
+        int firstHalf = FindTarget(nums, 0, pivot - 1, target);
+        if (firstHalf != -1) {
+            return firstHalf;
+        }
+        return FindTarget(nums, pivot + 1, nums.length - 1, target);
+
+    }
+
+    int FindPivotInArray(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (mid < right && nums[mid] > nums[mid + 1]) {
                 return mid;
-
-            if (nums[i] <= nums[mid]) {
-                if (nums[i] <= target && target < nums[mid]) {
-                    j = mid - 1;
-                } else {
-                    i = mid + 1;
-                }
-
             }
 
-            else {
-                if (nums[mid] <= target && target <= nums[j]) {
-                    i = mid + 1;
-                } else {
-                    j = mid - 1;
+            if (mid > left && nums[mid] < nums[mid - 1]) {
+                return mid - 1;
+            }
 
-                }
+            if (nums[mid] >= nums[left]) {
+                left = mid + 1;
+
+            } else {
+                right = mid - 1;
+            }
+
+        }
+        return -1;
+    }
+
+    int FindTarget(int[] nums, int left, int right, int target) {
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int value = nums[mid];
+
+            if (value == target) {
+                return mid;
+            }
+
+            if (value < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
 
         }
