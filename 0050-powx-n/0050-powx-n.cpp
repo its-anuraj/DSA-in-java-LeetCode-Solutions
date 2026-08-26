08c65282391aed1d0b811ec041400b84d81dea13
Anuraj singh <ajsinghindolia@gmail.com>
2026-08-26 21:15:10 +0530

    Time: 0 ms (100%), Space: 8.6 MB (44.57%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans = 1;
+
+        long power = n;
+
+        if (power < 0) {
+            x = 1 / x;
+            power = -power;
+        }
+
+        while (power > 0) {
+            if ((power & 1) == 1) {
+                ans *= x;
+            }
+            x *= x;
+            power = power >> 1;
+        }
+        return ans;
+    }
+};
